6e8f3429310b40fadcce788c5c19c7c43434d786
Oksana Danylenko <danylenko.ok@gmail.com>
2020-07-29 17:51:20 +0300

    add templates

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-const http = require('http');
-const bodyParser = require('body-parser');
-const express = require('express');
 const path = require('path');
 
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
-const server = http.createServer(app);
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop')
-
-app.use(express.static(path.join(__dirname, 'public')))
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin',adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
-app.use((req, res, next)=> {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
-})
+app.use((req, res, next) => {
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
+});
 
-server.listen(3000);
+app.listen(3002);
