f107da4a380c2217cf91a69dc3e3a762c947db27
Oksana Danylenko <danylenko.ok@gmail.com>
2020-08-26 16:26:11 +0300

    add order model

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const Product = require('./models/product');
 const User = require('./models/user');
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 const app = express();
 
@@ -43,6 +45,9 @@ Cart.belongsTo(User); //optional - previous string is enough
 Cart.belongsToMany(Product, { through: CartItem });
 //through - tell where the connection should be stored
 Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
 
 sequelize
   .sync()
