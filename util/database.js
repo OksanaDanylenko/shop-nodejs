997c17251aa7543070ec2f2b1893204b52b2f931
Oksana Danylenko <danylenko.ok@gmail.com>
2020-08-26 11:57:04 +0300

    add sequilize methonds - create, findAll, findByPk, save,destroy

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize('node-complete', 'root', 'wami124355', {
+  dialect: 'mysql',
   host: 'localhost',
-  user: 'root',
-  database: 'node-complete',
-  password: 'wami124355',
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
