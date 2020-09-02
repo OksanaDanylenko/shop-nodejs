a5a823966a0801d0d10749869ebd34243dc3a1f7
Oksana Danylenko <danylenko.ok@gmail.com>
2020-09-02 14:26:17 +0300

    add cookies

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI =
+  'mongodb+srv://oksana:lhz13YDaxSXm5LJR@cluster0.hymvn.mongodb.net/shop? w=majority';
+
 const app = express();
+const store = new MongoDBStore({ uri: MONGODB_URI, collection: 'sessions ' });
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -19,11 +25,24 @@ const authRoutes = require('./routes/auth');
 app.use(bodyParser.urlencoded({ extended: false }));
 // eslint-disable-next-line no-undef
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  }),
+);
+//resave - session will not be saved on every request that is one
 
 app.use((req, res, next) => {
-  User.findById('5f48d7658c4aa87ed009e56f')
+  if (!req.session.user) {
+    return next();
+  }
+
+  User.findById(req.session.user._id)
     .then((user) => {
-      req.user = user;
+      req.user = user; //store mogoose methods for user
       next();
     })
     .catch((err) => console.log(err)); // this code runs for upcoming requests only
@@ -36,9 +55,7 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-    'mongodb+srv://oksana:lhz13YDaxSXm5LJR@cluster0.hymvn.mongodb.net/shop?retryWrites=true&w=majority',
-  )
+  .connect(MONGODB_URI)
   .then(() => {
     User.findOne().then((user) => {
       if (!user) {
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,27 @@
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
+  req.session.isLoggedIn;
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
+    isAuthenticated: req.session.isLoggedIn,
   });
+};
+
+exports.postLogin = (req, res, next) => {
+  User.findById('5f48d7658c4aa87ed009e56f')
+    .then((user) => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      req.session.save((err) => {
+        console.log(err);
+        res.redirect('/');
+      }); // ensure that session created before continue
+    })
+    .catch((err) => console.log(err)); // this code runs for upcoming requests only
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(() => res.redirect('/'));
 };
