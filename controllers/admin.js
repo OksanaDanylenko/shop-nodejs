5c1b72c80cf2f7b4a30354a8ce39f9f7def1911b
Oksana Danylenko <danylenko.ok@gmail.com>
2020-10-06 12:41:58 +0300

    add async query

--- controllers/admin.js
+++ controllers/admin.js
@@ -189,22 +189,20 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
   Product.findById(prodId)
     .then((product) => {
       if (!product) {
         return next(new Error('Product not found'));
       }
       fileHelper.deleteFile(product.imageUrl);
-      Product.deleteOne({ _id: prodId, userId: req.user._id }); //findByIdAndDelete - mongoose
+      return Product.deleteOne({ _id: prodId, userId: req.user._id }); //findByIdAndDelete - mongoose
     })
     .then(() => {
-      res.redirect('/admin/products');
+      res.status(200).json({ message: 'Success' });
     })
     .catch((err) => {
-      const error = new Error(err);
-      error.httpStatusCode = 500;
-      return next(error);
+      res.status(500).json({ message: 'Deleting product failed' });
     });
 };
